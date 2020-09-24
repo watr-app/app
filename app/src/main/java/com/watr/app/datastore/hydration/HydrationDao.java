3b3a276bf168af78c50d82db55d79e92d15fdbbe
Linus Willner <hello@linuswillner.me>
2020-09-24 10:56:11 +0300

    Add copyright notices

--- app/src/main/java/com/watr/app/datastore/hydration/HydrationDao.java
+++ app/src/main/java/com/watr/app/datastore/hydration/HydrationDao.java
@@ -1,3 +1,9 @@
+/*
+ * Watr. Android application
+ * Copyright (c) 2020 Linus Willner, Panu Eronen and Markus Hartikainen.
+ * All rights reserved.
+ */
+
 package com.watr.app.datastore.hydration;
 
 import androidx.lifecycle.LiveData;
