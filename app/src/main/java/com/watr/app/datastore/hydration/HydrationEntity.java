0d7a53d13229c685e522e85cf28f31a2f2c8deff
Linus Willner <hello@linuswillner.me>
2020-09-24 11:55:41 +0300

    Add drink type to hydration entity and signify amount as absolute

--- app/src/main/java/com/watr/app/datastore/hydration/HydrationEntity.java
+++ app/src/main/java/com/watr/app/datastore/hydration/HydrationEntity.java
@@ -8,6 +8,7 @@ package com.watr.app.datastore.hydration;
 
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
+import com.watr.app.hydration.DrinkType;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -15,13 +16,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Entity(tableName = "hydration_records")
 public class HydrationEntity {
+
   @Getter
   @PrimaryKey(autoGenerate = true)
   private int id;
 
   @Getter
   @NonNull
-  private final double amount;
+  private final DrinkType drinkType;
+
+  @Getter
+  @NonNull
+  private final double amount; // I.e. absolute amount of liquid ingested
 
   @Getter
   @NonNull
---
+++ app/src/main/java/com/watr/app/hydration/DrinkType.java
@@ -0,0 +1,30 @@
+/*
+ * Watr. Android application
+ * Copyright (c) 2020 Linus Willner, Panu Eronen and Markus Hartikainen.
+ * All rights reserved.
+ */
+
+package com.watr.app.hydration;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum DrinkType {
+  WATER("Water", 1.0),
+  WATER_CARBONATED("Water (Carbonated)", 0.85),
+  WATER_FLAVOURED("Water (Flavoured)", 0.75),
+  MILK("Milk", 0.8),
+  TEA("Tea", 0.8),
+  COFFEE("Coffee", 0.7),
+  JUICE("Juice", 0.6),
+  HOT_CHOCOLATE("Hot chocolate", 0.5),
+  SOFT_DRINK("Soft drink", 0.4),
+  ENERGY_DRINK("Energy drink", 0.4),
+  BEER("Beer",-1.0),
+  WINE("Wine", -1.0),
+  CHAMPAGNE("Champagne", -2.0),
+  SPIRIT("Spirit", -4.0);
+
+  public final String label;
+  public final double hydrationCoefficient;
+}
