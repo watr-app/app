788f6f6b7a635466e3e1349d1c99a4e4da9ec529
Linus Willner <hello@linuswillner.me>
2020-09-28 17:34:25 +0300

    Add Gson to stringifier utils

---
+++ app/src/main/java/com/watr/app/utils/StringifyUtils.java
@@ -0,0 +1,13 @@
+/*
+ * Watr. Android application
+ * Copyright (c) 2020 Linus Willner, Panu Eronen and Markus Hartikainen.
+ * All rights reserved.
+ */
+
+package com.watr.app.utils;
+
+import com.google.gson.Gson;
+
+public class StringifyUtils {
+  public static Gson gson = new Gson();
+}
