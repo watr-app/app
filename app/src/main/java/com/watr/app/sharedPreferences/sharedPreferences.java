b809b13861fa3de8483e21b131028b9fe645bbab
Markus <markuaha@metropolia.fi>
2020-09-28 20:20:19 +0300

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/watr/app/sharedPreferences/sharedPreferences.java
+++ app/src/main/java/com/watr/app/sharedPreferences/sharedPreferences.java
@@ -5,30 +5,27 @@
  */
 
 package com.watr.app.sharedPreferences;
+
 import static android.content.Context.MODE_PRIVATE;
+
 import android.content.SharedPreferences;
-import android.widget.TextView;
 
 
+ 
 public class sharedPreferences {
+
   private static final String SHARED_PREFS = "sharedPrefs";
   private static final String SEX = "sex";
   private static final String WEIGHT = "weight";
 
 
-
-  public void saveData(Boolean sex, int weight) {
+  public void saveData(boolean sex, int weight) {
     SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
     SharedPreferences.Editor editor = sharedPreferences.edit();
 
-    editor.putBoolean(SEX, sex);
-    editor.putInt(WEIGHT, weight);
     editor.apply();
   }
 
-  public void loadData() {
-    SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-    sharedPreferences.getString(SEX, "");
-    sharedPreferences.getInt(WEIGHT, -1);
+    Boolean sex = sharedPreferences.getBoolean(SEX, false);
   }
 }
