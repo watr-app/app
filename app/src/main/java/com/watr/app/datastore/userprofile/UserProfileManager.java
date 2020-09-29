ac5f783d36d55399864df837913164d81331afc3
Markus <markuaha@metropolia.fi>
2020-09-29 18:15:38 +0300

    UserProfileManager.java
    edit

--- app/src/main/java/com/watr/app/datastore/userprofile/UserProfileManager.java
+++ app/src/main/java/com/watr/app/datastore/userprofile/UserProfileManager.java
@@ -5,24 +5,25 @@
  */
 
 package com.watr.app.datastore.userprofile;
-
-import static android.content.Context.MODE_PRIVATE;
-
+// import static android.content.Context.MODE_PRIVATE;
 import android.content.SharedPreferences;
+import com.watr.app.hydration.Gender;
+
 
 public class UserProfileManager {
 
-  private static final String SHARED_PREFS = "sharedPrefs";
-  private static final String SEX = "sex";
-  private static final String WEIGHT = "weight";
 
-  public void saveData(boolean sex, int weight) {
-    SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-    SharedPreferences.Editor editor = sharedPreferences.edit();
 
-    editor.apply();
+  // sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+  public void addSex(SharedPreferences sharedPreferences, String key, int sex) {
+
+    SharedPreferences.Editor prefEditor = sharedPreferences.edit();
+    prefEditor.putInt(key, sex);
+    prefEditor.apply();
   }
 
-  Boolean sex = sharedPreferences.getBoolean(SEX, false);
+  public int getSex(SharedPreferences sharedPreferences, String key) {
 
+    return sharedPreferences.getInt(key, 0);
+  }
 }
