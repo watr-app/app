34d92d3f98e682819f2aa08a307b6161ac5207a2
Linus Willner <hello@linuswillner.me>
2020-10-01 21:30:45 +0300

    Add ActivityPeriod constant

---
+++ app/src/main/java/com/watr/app/constants/ActivityPeriod.java
@@ -0,0 +1,18 @@
+/*
+ * Watr. Android application
+ * Copyright (c) 2020 Linus Willner, Panu Eronen and Markus Hartikainen.
+ * All rights reserved.
+ */
+
+package com.watr.app.constants;
+
+/**
+ * Constants for activity periods (I.e. when user is supposed to be awake or asleep).
+ *
+ * @author linuswillner
+ * @version 1.0.0
+ */
+public enum ActivityPeriod {
+  AWAKE,
+  ASLEEP
+}
