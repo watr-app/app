8fa1a7fa1624f4c781be537772f8b71ae255bb29
Linus Willner <hello@linuswillner.me>
2020-10-02 14:56:05 +0300

    Slightly refactor ActivityPeriodManager

--- app/src/main/java/com/watr/app/timemgmt/ActivityPeriodManager.java
+++ app/src/main/java/com/watr/app/timemgmt/ActivityPeriodManager.java
@@ -10,20 +10,45 @@ import com.watr.app.MainActivity;
 import com.watr.app.constants.ActivityPeriod;
 import com.watr.app.constants.DateOffset;
 import com.watr.app.datastore.sharedpreferences.userprofile.UserProfileManager;
-import java.time.LocalTime;
 import lombok.val;
 
+/**
+ * Activity period management and calculation utilities.
+ *
+ * @author linuswillner
+ * @version 1.0.0
+ */
 public class ActivityPeriodManager {
   private static UserProfileManager userProfileManager = MainActivity.getUserProfileManager();
 
+  /**
+   * Returns the current activity period.
+   *
+   * @return {@link ActivityPeriod}
+   * @throws UnknownTimeIntervalException If time interval wasn't wake => bed or bed => wake (Which
+   *     should be downright physically impossible).
+   */
   public static ActivityPeriod getCurrentActivityPeriod() throws UnknownTimeIntervalException {
-    return checkActivityPeriod();
+    return checkActivityPeriod().activityPeriod;
   }
 
-  private static ActivityPeriod checkActivityPeriod() throws UnknownTimeIntervalException {
+  /**
+   * Checks the current activity period, compensating for times potentially hitting across date
+   * boundaries. Additionally returns the calculated offsets for the wake time and bed time.
+   *
+   * @return {@link ActivityPeriodWithOffsets}
+   * @throws UnknownTimeIntervalException if time interval was not wake => bed or bed => wake (Which
+   *     should be downright physically impossible).
+   */
+  private static ActivityPeriodWithOffsets checkActivityPeriod()
+      throws UnknownTimeIntervalException {
     val wakeTime = userProfileManager.getWakeTime();
     val bedTime = userProfileManager.getBedTime();
 
+    DateOffset wakeTimeOffset = DateOffset.TODAY;
+    DateOffset bedTimeOffset = DateOffset.TODAY;
+
+    // Figure out what the current activity period is
     // Going to from here on in abbreviate wake time as W and bed time as B to save space
 
     // Is between wake time and bed time today
@@ -35,8 +60,6 @@ public class ActivityPeriodManager {
     val isBetweenWYesterdayAndBToday =
         TimeUtils.currentTimeIsInterval(wakeTime, DateOffset.YESTERDAY, bedTime, DateOffset.TODAY);
 
-    val isAwake = isBetweenWB || (isBetweenWTodayAndBTomorrow || isBetweenWYesterdayAndBToday);
-
     // Is between bed time and wake time today
     val isBetweenBW = TimeUtils.currentTimeIsInInterval(bedTime, wakeTime);
     // Is between bed time yesterday and wake time today
@@ -46,34 +69,10 @@ public class ActivityPeriodManager {
     val isBetweenBTodayAndWTomorrow =
         TimeUtils.currentTimeIsInterval(bedTime, DateOffset.TODAY, wakeTime, DateOffset.TOMORROW);
 
-    val isAsleep = isBetweenBW || (isBetweenBYesterdayAndWToday || isBetweenBTodayAndWTomorrow);
-
-    if (isAwake) {
-      return ActivityPeriod.AWAKE;
-    } else if (isAsleep) {
-      return ActivityPeriod.ASLEEP;
-    } else {
-      // Throwing here because the only possible states to be in are the ones defined above; time
-      // can physically speaking only be during one of those periods. In other words, if this
-      // throws, something is really, really, *really* wrong.
-      throw new UnknownTimeIntervalException("Time interval wasn't wake => bed or bed => wake");
-    }
-  }
-
-  public static ActivityPeriodOffset getActivityPeriodOffsets(
-      LocalTime wakeTime, LocalTime bedTime) {
-    DateOffset wakeTimeOffset = DateOffset.TODAY;
-    DateOffset bedTimeOffset = DateOffset.TODAY;
-
-    // Abbreviating wake time as W and bed time as B to save space
+    // Make the conclusions
 
     // Wake time offset
     // Not checking for W => B as that's the default condition
-    val isBetweenWTodayAndBTomorrow =
-        TimeUtils.currentTimeIsInterval(wakeTime, DateOffset.TODAY, bedTime, DateOffset.TOMORROW);
-    val isBetweenWYesterdayAndBToday =
-        TimeUtils.currentTimeIsInterval(wakeTime, DateOffset.YESTERDAY, bedTime, DateOffset.TODAY);
-
     if (isBetweenWTodayAndBTomorrow) {
       wakeTimeOffset = DateOffset.TODAY;
       bedTimeOffset = DateOffset.TOMORROW;
@@ -84,11 +83,6 @@ public class ActivityPeriodManager {
 
     // Bed time offset
     // Not checking for B => W as that's the default condition
-    val isBetweenBYesterdayAndWToday =
-        TimeUtils.currentTimeIsInterval(bedTime, DateOffset.YESTERDAY, wakeTime, DateOffset.TODAY);
-    val isBetweenBTodayAndWTomorrow =
-        TimeUtils.currentTimeIsInterval(bedTime, DateOffset.TODAY, wakeTime, DateOffset.TOMORROW);
-
     if (isBetweenBYesterdayAndWToday) {
       wakeTimeOffset = DateOffset.TODAY;
       bedTimeOffset = DateOffset.YESTERDAY;
@@ -97,6 +91,20 @@ public class ActivityPeriodManager {
       bedTimeOffset = DateOffset.TODAY;
     }
 
-    return new ActivityPeriodOffset(wakeTimeOffset, bedTimeOffset);
+    val isAwake = isBetweenWB || (isBetweenWTodayAndBTomorrow || isBetweenWYesterdayAndBToday);
+    val isAsleep = isBetweenBW || (isBetweenBYesterdayAndWToday || isBetweenBTodayAndWTomorrow);
+
+    // Return the result
+
+    if (isAwake) {
+      return new ActivityPeriodWithOffsets(ActivityPeriod.AWAKE, wakeTimeOffset, bedTimeOffset);
+    } else if (isAsleep) {
+      return new ActivityPeriodWithOffsets(ActivityPeriod.ASLEEP, wakeTimeOffset, bedTimeOffset);
+    } else {
+      // Throwing here because the only possible states to be in are the ones defined above; time
+      // can physically speaking only be during one of those periods. In other words, if this
+      // throws, something is really, really, *really* wrong.
+      throw new UnknownTimeIntervalException("Time interval wasn't wake => bed or bed => wake");
+    }
   }
 }
--- app/src/main/java/com/watr/app/timemgmt/ActivityPeriodWithOffsets.java
+++ app/src/main/java/com/watr/app/timemgmt/ActivityPeriodWithOffsets.java
@@ -7,11 +7,13 @@
 package com.watr.app.timemgmt;
 
 
+import com.watr.app.constants.ActivityPeriod;
 import com.watr.app.constants.DateOffset;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class ActivityPeriodOffset {
+public class ActivityPeriodWithOffsets {
+  public final ActivityPeriod activityPeriod;
   public final DateOffset wakeTimeOffset;
   public final DateOffset bedTimeOffset;
 }
