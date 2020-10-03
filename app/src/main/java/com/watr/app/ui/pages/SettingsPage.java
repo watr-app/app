385b52ed266218775bcb909accc05a5c26fe4c4c
panueronen <64413759+panueronen@users.noreply.github.com>
2020-10-04 02:22:19 +0300

    added oncreatedview(), and test array with strings
    added oncreatedview(), and test array with strings

--- app/src/main/java/com/watr/app/ui/pages/SettingsPage.java
+++ app/src/main/java/com/watr/app/ui/pages/SettingsPage.java
@@ -10,8 +10,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.watr.app.R;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.NoArgsConstructor;
 
 /**
@@ -22,10 +28,29 @@ import lombok.NoArgsConstructor;
  */
 @NoArgsConstructor
 public class SettingsPage extends Fragment {
+  private View view;
+  List<String> settings = new ArrayList<String>();
+
 
   @Override
   public View onCreateView(
       LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     return inflater.inflate(R.layout.fragment_settings, container, false);
   }
+
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    settings.add("Metric System");
+    settings.add("24h Clock");
+    settings.add("Edit user profile");
+    this.view = view;
+    ListView lv = view.findViewById(R.id.settingslist);
+    lv.setAdapter(new ArrayAdapter<String>(
+        getContext(),android.R.layout.simple_list_item_1,
+        settings
+    ));
+
+    }
+
+
 }
