21e2dd60ca2fcb91d23a8c60d42f76354870c628
panueronen <64413759+panueronen@users.noreply.github.com>
2020-10-04 04:28:26 +0300

    added new activities for settings and userprofilesettings
    added new activities for settings and userprofilesettings, and list view that holds 2 items

---
+++ app/src/main/java/com/watr/app/UserProfileSettings.java
@@ -0,0 +1,19 @@
+/*
+ * Watr. Android application
+ * Copyright (c) 2020 Linus Willner, Panu Eronen and Markus Hartikainen.
+ * All rights reserved.
+ */
+
+package com.watr.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserProfileSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_settings);
+    }
+}
--- app/src/main/java/com/watr/app/ui/pages/SettingsPage.java
+++ app/src/main/java/com/watr/app/ui/pages/SettingsPage.java
@@ -6,19 +6,29 @@
 
 package com.watr.app.ui.pages;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.watr.app.Appsettings;
 import com.watr.app.R;
+import com.watr.app.UserProfileSettings;
+import com.watr.app.datastore.sharedpreferences.settings.SettingsManager;
+import com.watr.app.datastore.sharedpreferences.userprofile.UserProfileManager;
+import com.watr.app.ui.activities.MainActivity;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.NoArgsConstructor;
+import lombok.val;
 
 /**
  * Settings page. All functionality specific to the settings page goes here.
@@ -29,7 +39,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SettingsPage extends Fragment {
   private View view;
+  private String opt1,opt2;
+  String EXTRA;
   List<String> settings = new ArrayList<String>();
+
 
 
   @Override
@@ -40,8 +53,7 @@ public class SettingsPage extends Fragment {
 
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    settings.add("Metric System");
-    settings.add("24h Clock");
+    settings.add("App settings");
     settings.add("Edit user profile");
     this.view = view;
     ListView lv = view.findViewById(R.id.settingslist);
@@ -49,7 +61,23 @@ public class SettingsPage extends Fragment {
         getContext(),android.R.layout.simple_list_item_1,
         settings
     ));
-
+    lv.setOnItemClickListener(
+        new OnItemClickListener() {
+          @Override
+          public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+            Log.d("Tag", "onItemClick(" + i + ")");
+            if (i == 0) {
+              Intent nextActivity = new Intent(getContext(), Appsettings.class);
+              nextActivity.putExtra(EXTRA, i);
+              startActivity(nextActivity);
+            }else{
+              Intent nextActivity = new Intent(getContext(), UserProfileSettings.class);
+              nextActivity.putExtra(EXTRA, i);
+              startActivity(nextActivity);
+              
+            }
+          }
+        });
     }
 
 
